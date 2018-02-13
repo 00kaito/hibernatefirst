607fc257a38a8f008c1b19306370f9f574939a79
00kaito <bednarz@programmer.net>
2018-02-13 19:52:03 +0100

    Initial commit

---
+++ src/main/java/com/akademiakodu/mysql/hibernatefirst/HibernatefirstApplication.java
@@ -0,0 +1,12 @@
+package com.akademiakodu.mysql.hibernatefirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatefirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatefirstApplication.class, args);
+	}
+}
---
+++ src/main/java/com/akademiakodu/mysql/hibernatefirst/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.akademiakodu.mysql.hibernatefirst.repository;
+
+import com.akademiakodu.mysql.hibernatefirst.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+
+
+}
